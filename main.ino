8b994bce5c9b41edd5703c074fcf31a3495bfd5c
Dylan K <50585244+dylankainth@users.noreply.github.com>
2024-01-20 09:00:34 +0000

    added wifi + ping to beeceptor v1

--- main.ino
+++ main.ino
@@ -1,6 +1,6 @@
-#include <SoftwareSerial.h>
-#include <MHZ.h>
-#include <PMserial.h>
+#include <SoftwareSerial.h> // https://github.com/plerup/espsoftwareserial
+#include <MHZ.h> // https://github.com/tobiasschuerg/MH-Z-CO2-Sensors v1.4.0 ONLY, edit librar
+#include <PMserial.h> // https://github.com/avaldebe/PMserial
 
 // pin for pwm reading
 #define CO2_IN 10
@@ -8,6 +8,14 @@
 // pin for uart reading
 #define MH_Z19_RX 17  // D7
 #define MH_Z19_TX 16  // D6
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "ESP32Connect";
+const char* password = "hellothere";
+
+String serverName = "http://badaltest.free.beeceptor.com/";
 
 SerialPM pms(PMSx003, 32, 27);  // PMSx003, RX, TX
 MHZ co2(MH_Z19_RX, MH_Z19_TX, CO2_IN, MHZ19B);
@@ -30,6 +38,17 @@ void setup() {
     }
     Serial.println();
   }
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
 }
 
 void loop() {
@@ -49,7 +68,40 @@ void loop() {
     Serial.print(ppm_uart);
   } else {
     Serial.print("n/a");
+    Serial.print(ppm_uart);
   }
 
   delay(5000);
+
+  if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+
+      String serverPath = serverName + "?temperature=24.37";
+      
+      // Your Domain name with URL path or IP address with path
+      http.begin(serverPath.c_str());
+      
+      // If you need Node-RED/server authentication, insert user and password below
+      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+      
+      // Send HTTP GET request
+      int httpResponseCode = http.GET();
+      
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    
 }
